ef6b68c7bf2b61b12d92a66a267be866d78e2bd5
Christian Montero Martínez <christianmontero08@gmail.com>
2020-06-07 13:50:15 -0700

    Updates

---
+++ precache-manifest.83b80290812cd8342b9d751ea10e3eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caf04691cb3452bf2e6b8f607a25d48",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "382dddeeea3a3e9450d9",
+    "url": "/rolodex/static/css/main.b71c0d18.chunk.css"
+  },
+  {
+    "revision": "3c9d29464f6976dfc6d0",
+    "url": "/rolodex/static/js/2.eeae8fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.eeae8fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382dddeeea3a3e9450d9",
+    "url": "/rolodex/static/js/main.053250e0.chunk.js"
+  },
+  {
+    "revision": "3727d9797b52f86e3208",
+    "url": "/rolodex/static/js/runtime-main.691a164e.js"
+  }
+]);
